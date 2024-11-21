d91edf47df4539d8cdf28a300732b0acfa187b5d
sindhu <padmasindhu7673@gmail.com>
2024-11-21 15:07:16 +0530

    demo

---
+++ src/main/java/SEgroup/MavenJavaProject/App.java
@@ -0,0 +1,12 @@
+package SEgroup.MavenJavaProject;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the first Maven Java Project in Eclipse shiva" );
+    }
+}
