597a8a53ea274bdf0fded90a903d26256f6222c0
Sindhupadma17 <sindhupadma1704@gmail.com>
2024-11-27 11:11:01 +0530

    Update App.java

--- src/main/java/SEgroup/MavenJavaProject/App.java
+++ src/main/java/SEgroup/MavenJavaProject/App.java
@@ -7,6 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is the first Maven Java Project in Eclipse shiva" );
+        System.out.println( "This is the first Maven Java Project in Eclipse sindhu" );
     }
 }
